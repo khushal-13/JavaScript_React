a24d0872a66543b4cb9390567c16b90889881130
Khushal Jogi <khushaljogi7@gmail.com>
2025-08-01 08:31:14 +0530

    React Components - 2

--- mega-blog/src/components/Header/Button.jsx
+++ mega-blog/src/components/Header/Button.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 
 function Button({
-    children
+    children,
+    type = 'button',
+    bgColor = 'bg-blue-600',
+    textColor = 'white',
+    className = '',
+    ...props
 }) {
   return (
-    <button>
+    <button className={`px-4 py-2 rounded-lg ${textColor} ${bgColor} ${className} `} {...props}>
         {children}
     </button>
   )
